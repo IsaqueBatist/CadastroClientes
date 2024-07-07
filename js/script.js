29eb2e8a4c103099cd9b5f7b2ea84c22f7c2be82
Isaque <isaquebatista400@gmail.com>
2024-07-07 00:51:41 -0300

    Corrigindo a reinicialização do formuário

--- js/script.js
+++ js/script.js
@@ -23,18 +23,29 @@ function checkInput() {
   }
 }
 
-async function getData(cep) {
+function setInputs(data, validation){
   const errordiv = document.getElementById("messageError")
-    const {data} = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
-    const {logradouro, bairro, localidade, uf } = data
-    
-    document.getElementById('adress').value = logradouro? logradouro: ''
-    document.getElementById('neighborhood').value = bairro? bairro: ''
-    document.getElementById('city').value = localidade? localidade: ''
-    document.getElementById('state').value = uf? uf: ''
-    uf? $("#housenumber").prop("disabled", false) : null
+  const {logradouro, bairro, localidade, uf } = data
+  if(validation){
+    document.getElementById('adress').value = logradouro
+    document.getElementById('neighborhood').value = bairro
+    document.getElementById('city').value = localidade
+    document.getElementById('state').value = uf
+    $("#housenumber").prop("disabled", false)
+  }else {
+    document.getElementById('adress').value = ""
+    document.getElementById('neighborhood').value = ""
+    document.getElementById('city').value = ""
+    document.getElementById('state').value = ""
+    $("#housenumber").prop("disabled", true)
 
-    uf? "" : errordiv.innerHTML = 'CEP não encontrado'
+    errordiv.innerHTML = 'CEP não encontrado'
+  }
+}
+
+async function getData(cep) {
+    const {data} = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+    data.cep? setInputs(data, true) : setInputs(data, false)
 }
 document.getElementById('formData').addEventListener('submit', function(event) {
   event.preventDefault()
