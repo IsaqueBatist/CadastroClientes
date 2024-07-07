5ccb96224bea3ea0c1ca63a0084af3e2661665c2
Isaque <isaquebatista400@gmail.com>
2024-07-07 00:17:16 -0300

    closes #8 - Preenchendo a tabela automaticamente

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+let customers = [
+  {
+    id: 1,
+    name: "Glauco Todesco",
+    adress: "	Rua Leite Penteado, 999",
+    cep: "18010-050",
+    neighborhood: "	Centro",
+    city: "Sorocaba",
+    stat: "SP",
+  }
+]
+
 $("#cepinput").mask("00000-000")
 function checkInput() {
   const cepinput = document.getElementById("cepinput").value
@@ -28,3 +40,41 @@ async function getData(cep) {
     errordiv.innerHTML = `CEP não encontrado`
   }
 }
+document.getElementById('formData').addEventListener('submit', function(event) {
+  event.preventDefault()
+
+  let newCustomers = {
+    id: customers.length + 1,
+    name: document.getElementById('iname').value + ' ' + document.getElementById('ilastname').value ,
+    adress: document.getElementById('adress').value + ', ' +document.getElementById('housenumber').value,
+    cep: document.getElementById("cepinput").value,
+    neighborhood:  document.getElementById("neighborhood").value,
+    city: document.getElementById("city").value,
+    stat: document.getElementById("state").value,
+  }
+  customers.push(newCustomers)
+  loadTable(customers)
+
+})
+
+function loadTable(data) {
+  const body = document.getElementById('databody')
+  body.innerHTML = ''
+  data.map((customer) => {
+    body.innerHTML += `
+      <tr>
+        <th scope="row">${customer.id}</th>
+        <td>${customer.name}</td>
+        <td>${customer.adress}</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>${customer.cep}</td>
+        <td>${customer.neighborhood}</td>
+        <td>${customer.city}</td>
+        <td>${customer.stat}</td>
+      </tr>
+    `
+  })
+}
+loadTable(customers)
